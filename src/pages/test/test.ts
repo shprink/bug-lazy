2f2a9a6989eef08b8441731359eff1645ca26a33
shprink <julien.renaux@gmail.com>
2017-06-20 22:25:03 +0200

    demonstrate the bug

---
+++ src/pages/test/test.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TestPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage({
+  segment: 'test/:id'
+})
+@Component({
+  selector: 'page-test',
+  templateUrl: 'test.html',
+})
+export class TestPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TestPage');
+  }
+
+}
